8f0aad845e2b6fd9be19302aab012fb4a6c29597
Leven Rochana <levenrok@proton.me>
2026-01-26 20:50:27 +0530

    implement improved database open and close functions

---
+++ src/database_v2.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#include "include/database_v2.h"
+
+Err openDatabaseV2(Database* db, const char* path) {
+    int rc = 0;
+    Err ret = OK;
+
+    db->path = path;
+    db->db = NULL;
+    db->err_msg = NULL;
+
+    rc = sqlite3_open_v2(path, &(db->db), SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+    if (rc != SQLITE_OK) {
+        db->err_msg = sqlite3_errmsg(db->db);
+
+        ret = ERR_DB_OPEN;
+        return ret;
+    }
+
+    sqlite3_extended_result_codes(db->db, 1);
+
+    return ret;
+}
+
+Err closeDatabaseV2(Database* db) {
+    int rc = sqlite3_close_v2(db->db);
+    sqlite3_free(&(db->err_msg));
+
+    if (rc != SQLITE_OK) {
+        db->db = NULL;
+        return ERR_DB_CLOSE;
+    }
+
+    db->db = NULL;
+    return OK;
+}
---
+++ src/include/database_v2.h
@@ -0,0 +1,17 @@
+#ifndef MIMIR_DATABASE_H
+#define MIMIR_DATABASE_H
+
+#include <sqlite3.h>
+
+#include "../utils/include/errs.h"
+
+typedef struct Database {
+    const char* path;
+    sqlite3* db;
+    const char* err_msg;
+} Database;
+
+Err openDatabaseV2(Database* db, const char* path);
+Err closeDatabaseV2(Database* db);
+
+#endif  // MIMIR_DATABASE_H
