52ac9e09f7cf9289c194475603be92c80a5deb80
Leven Rochana <levenrok@proton.me>
2026-02-07 00:10:50 +0530

    upgrade the database interaction functions

--- src/database.c
+++ src/database.c
@@ -4,90 +4,135 @@
 
 #include "include/database.h"
 
-#include "utils/include/fs.h"
 #include "utils/include/log.h"
 
-#define LOG_DB_ERR(db) logger(ERROR, "db", "%s", sqlite3_errmsg(db));
+#define LOG_DB_ERR(db) logger(ERROR, "db", "%s", sqlite3_errmsg(db))
 
 static const int CONTENT_SIZE = 1024;
 static const int SCRIPT_SIZE = (CONTENT_SIZE + 32);
 
-Err openDatabase(sqlite3** db) {
-    sqlite3_stmt* stmt;
+static inline void cleanup_sqlite(sqlite3** db) {
+    if (*db)
+        sqlite3_close(*db);
+}
 
-    char database_path[256];
+static inline void cleanup_stmt(sqlite3_stmt** stmt) {
+    if (*stmt)
+        sqlite3_finalize(*stmt);
+}
 
+static inline void cleanup_errmsg(char** zErrMsg) {
+    if (*zErrMsg)
+        sqlite3_free(*zErrMsg);
+}
+
+DbErr openDatabase(Database* db, const char* path) {
     int rc = 0;
-    char* zErrMsg = NULL;
 
-    Err ret = OK;
+    db->db = NULL;
+    db->path = path;
 
-    if (getAppDataPath(database_path, "db.sqlite3") != OK) {
-        STDOUT_LOGGER_ERROR("%s", "cannot access the database");
-
-        ret = ERR_DB_ACCESS;
-        goto err;
-    }
-
-    rc = sqlite3_open_v2(database_path, db, SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
+    rc = sqlite3_open_v2(path, &(db->db), SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE, NULL);
     if (rc != SQLITE_OK) {
-        STDOUT_LOGGER_ERROR("%s", "cannot open the database");
-
-        ret = ERR_DB_OPEN;
-        goto err;
+        LOG_DB_ERR(db->db);
+        return DB_ERR_OPEN;
     }
 
-    rc = sqlite3_exec(*db, "PRAGMA journal_mode = wal;", NULL, NULL, &zErrMsg);
-    if (rc != SQLITE_OK) {
-        logger(WARNING, "db", "%s", zErrMsg);
-    }
+    sqlite3_extended_result_codes(db->db, 1);
 
-    const char* sql =
+    return DB_OK;
+}
+
+DbErr initDatabase(Database* db) {
+#if defined(__GNUC__) || defined(__clang__)
+    sqlite3_stmt* stmt __attribute__((cleanup(cleanup_stmt)));
+    char* zErrMsg __attribute__((cleanup(cleanup_errmsg))) = NULL;
+#else
+    sqlite3_stmt* stmt;
+    char* zDbErrMsg = NULL;
+#endif
+    char* zSql =
         "CREATE TABLE IF NOT EXISTS scripts ("
         "id INTEGER PRIMARY KEY,"
         "name TEXT NOT NULL UNIQUE,"
         "shebang TEXT,"
         "content TEXT NOT NULL);";
 
-    rc = sqlite3_prepare_v2(*db, sql, -1, &stmt, NULL);
+    DbErr ret = DB_OK;
+
+    int rc = sqlite3_exec(db->db, "PRAGMA journal_mode = WAL;", NULL, NULL, &zErrMsg);
     if (rc != SQLITE_OK) {
-        STDOUT_LOGGER_ERROR("%s", "cannot create the database table");
-
-        sqlite3_finalize(stmt);
-        ret = ERR_DB_CREATE_TABLE;
+        logger(ERROR, "db", "%s", zErrMsg);
+        ret = DB_ERR_INIT;
         goto err;
     }
 
-    rc = sqlite3_step(stmt);
-    if (rc != SQLITE_DONE) {
-        STDOUT_LOGGER_ERROR("%s", "cannot create the database table");
-
-        sqlite3_finalize(stmt);
-        ret = ERR_DB_CREATE_TABLE;
+    rc = sqlite3_exec(db->db, zSql, NULL, NULL, &zErrMsg);
+    if (rc != SQLITE_OK) {
+        LOG_DB_ERR(db->db);
+        ret = DB_ERR_CREATE_TABLE;
         goto err;
     }
 
-    sqlite3_extended_result_codes(*db, 1);
-
-    sqlite3_finalize(stmt);
-    sqlite3_free(zErrMsg);
+#if !defined(__GNUC__) || !defined(__clang__)
+    cleanup_errmsg(&zDbErrMsg);
+    cleanup_stmt(&stmt);
+#endif
     return ret;
 
 err:
-    LOG_DB_ERR(*db);
-    sqlite3_free(zErrMsg);
+#if !defined(__GNUC__) || !defined(__clang__)
+    cleanup_errmsg(&zDbErrMsg);
+    cleanup_stmt(&stmt);
+#endif
     return ret;
 }
 
-Err insertScript(sqlite3* db, char* name, char* content, char* shebang) {
+bool pingDatabase(const char* path) {
+#if defined(__GNUC__) || defined(__clang__)
+    sqlite3* db __attribute__((cleanup(cleanup_sqlite)));
+    sqlite3_stmt* stmt __attribute__((cleanup(cleanup_stmt)));
+#else
+    sqlite3* db;
     sqlite3_stmt* stmt;
-    const char* sql = "INSERT INTO scripts (name, shebang, content) VALUES (?, ?, ?);";
+#endif
 
-    Err ret = OK;
-
-    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK)
+    if (sqlite3_open_v2(path, &db, SQLITE_OPEN_READONLY, NULL) != SQLITE_OK)
         goto err;
+    if (sqlite3_prepare_v2(db, "SELECT 1;", -1, &stmt, NULL) != SQLITE_OK)
+        goto err;
+    if (sqlite3_step(stmt) != SQLITE_ROW)
+        goto err;
+
+#if !defined(__GNUC__) || !defined(__clang__)
+    cleanup_stmt(&stmt);
+    cleanup_sqlite(&db);
+#endif
+    return true;
+
+err:
+#if !defined(__GNUC__) || !defined(__clang__)
+    cleanup_stmt(&stmt);
+    cleanup_sqlite(&db);
+#endif
+    return false;
+}
+
+DbErr insertScript(Database* db, char* name, char* content, char* shebang) {
+#if defined(__GNUC__) || defined(__clang__)
+    sqlite3_stmt* stmt __attribute__((cleanup(cleanup_stmt)));
+#else
+    sqlite3_stmt* stmt;
+#endif
+    const char* zSql = "INSERT INTO scripts (name, shebang, content) VALUES (?, ?, ?);";
+
+    DbErr ret = DB_OK;
+
+    int rc = sqlite3_prepare_v2(db->db, zSql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        ret = DB_ERR;
+        goto err;
+    }
 
     sqlite3_bind_text(stmt, 1, name, -1, SQLITE_STATIC);
     sqlite3_bind_text(stmt, 2, shebang, -1, SQLITE_STATIC);
@@ -98,32 +143,44 @@ Err insertScript(sqlite3* db, char* name, char* content, char* shebang) {
         switch (rc) {
             case SQLITE_CONSTRAINT_UNIQUE:
                 STDOUT_LOGGER_ERROR("script named '%s' already exists!", name);
-                ret = ERR_DB_INSERT_UNIQUE;
+                ret = DB_ERR_INSERT_UNIQUE;
                 goto err;
             default:
-                ret = ERR_DB_INSERT;
+                ret = DB_ERR_INSERT;
                 goto err;
         }
     }
 
     STDOUT_LOGGER_SUCCESS("saved script '%s' to the database", name);
 
-    sqlite3_finalize(stmt);
+#if !defined(__GNUC__) || !defined(__clang__)
+    cleanup_stmt(&stmt);
+#endif
     return ret;
 
 err:
-    LOG_DB_ERR(db);
-    sqlite3_finalize(stmt);
+    LOG_DB_ERR(db->db);
+#if !defined(__GNUC__) || !defined(__clang__)
+    cleanup_stmt(&stmt);
+#endif
     return ret;
 }
 
-Err getScripts(sqlite3* db) {
+DbErr getScripts(Database* db) {
+#if defined(__GNUC__) || defined(__clang__)
+    sqlite3_stmt* stmt __attribute__((cleanup(cleanup_stmt)));
+#else
     sqlite3_stmt* stmt;
-    const char* sql = "SELECT name, shebang, content FROM scripts;";
+#endif
+    const char* zSql = "SELECT name, shebang, content FROM scripts;";
 
-    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
-    if (rc != SQLITE_OK)
+    DbErr ret = DB_OK;
+
+    int rc = sqlite3_prepare_v2(db->db, zSql, -1, &stmt, NULL);
+    if (rc != SQLITE_OK) {
+        ret = DB_ERR;
         goto err;
+    }
 
     while ((rc = sqlite3_step(stmt)) == SQLITE_ROW) {
         const char* name = (const char*)sqlite3_column_text(stmt, 0);
@@ -136,29 +193,35 @@ Err getScripts(sqlite3* db) {
         printf("\033[1mcontent:\033[0m |\n%s", content);
     }
 
-    if (rc != SQLITE_DONE)
+    if (rc != SQLITE_DONE) {
+        ret = DB_ERR_SELECT;
         goto err;
+    }
 
+#if !defined(__GNUC__) || !defined(__clang__)
     sqlite3_finalize(stmt);
-    return OK;
+#endif
+    return ret;
 
 err:
     STDOUT_LOGGER_ERROR("%s", "cannot get scripts from the database!");
-    LOG_DB_ERR(db);
+    LOG_DB_ERR(db->db);
+#if !defined(__GNUC__) || !defined(__clang__)
     sqlite3_finalize(stmt);
-    return ERR_DB_SELECT;
+#endif
+    return ret;
 }
 
-Err getScriptContent(sqlite3* db, char* name, char* buffer, bool get_shebang) {
+DbErr getScriptContent(Database* db, char* name, char* buffer, bool get_shebang) {
     sqlite3_stmt* stmt;
-    const char* sql = get_shebang ? "SELECT shebang, content FROM scripts WHERE name = ?;"
-                                  : "SELECT shebang, content FROM scripts WHERE name = ?;";
+    const char* zSql = get_shebang ? "SELECT shebang, content FROM scripts WHERE name = ?;"
+                                   : "SELECT shebang, content FROM scripts WHERE name = ?;";
 
-    Err ret = OK;
+    DbErr ret = DB_OK;
 
-    int rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    int rc = sqlite3_prepare_v2(db->db, zSql, -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
-        ret = ERR_DB_SELECT;
+        ret = DB_ERR;
         goto err;
     }
 
@@ -169,11 +232,11 @@ Err getScriptContent(sqlite3* db, char* name, char* buffer, bool get_shebang) {
         switch (rc) {
             case SQLITE_DONE:
                 STDOUT_LOGGER_ERROR("no script named '%s' found in the database!", name);
-                ret = ERR_DB_SELECT_NO_MATCH;
+                ret = DB_ERR_SELECT_NO_MATCH;
                 goto err;
             default:
                 STDOUT_LOGGER_ERROR("cannot get contents of '%s' from the database!", name);
-                ret = ERR_DB_SELECT;
+                ret = DB_ERR_SELECT;
                 goto err;
         }
 
@@ -201,23 +264,27 @@ Err getScriptContent(sqlite3* db, char* name, char* buffer, bool get_shebang) {
     return ret;
 
 err:
-    LOG_DB_ERR(db);
+    LOG_DB_ERR(db->db);
     sqlite3_finalize(stmt);
     return ret;
 }
 
-Err deleteScript(sqlite3* db, char* name) {
+DbErr deleteScript(Database* db, char* name) {
+#if defined(__GNUC__) || defined(__clang__)
+    sqlite3_stmt* stmt __attribute__((cleanup(cleanup_stmt)));
+#else
     sqlite3_stmt* stmt;
-    const char* sql = "DELETE FROM scripts WHERE scripts.name = ?;";
+#endif
+    const char* zSql = "DELETE FROM scripts WHERE scripts.name = ?;";
 
     int rc = 0;
 
-    Err ret = OK;
+    DbErr ret = DB_OK;
 
-    rc = sqlite3_prepare_v2(db, sql, -1, &stmt, NULL);
+    rc = sqlite3_prepare_v2(db->db, zSql, -1, &stmt, NULL);
     if (rc != SQLITE_OK) {
-        ret = ERR_DB_DELETE;
-        LOG_DB_ERR(db);
+        ret = DB_ERR;
+        LOG_DB_ERR(db->db);
         goto err;
     }
 
@@ -225,33 +292,44 @@ Err deleteScript(sqlite3* db, char* name) {
 
     rc = sqlite3_step(stmt);
     if (rc == SQLITE_DONE) {
-        int changes = sqlite3_changes(db);
+        int changes = sqlite3_changes(db->db);
 
         if (changes <= 0) {
             STDOUT_LOGGER_ERROR("cannot find script '%s' to delete", name);
 
-            ret = ERR_DB_DELETE;
+            ret = DB_ERR_DELETE_NO_MATCH;
             goto err;
         }
     } else {
-        ret = ERR_DB_DELETE;
-        LOG_DB_ERR(db);
+        ret = DB_ERR_DELETE;
+        LOG_DB_ERR(db->db);
         goto err;
     }
 
     STDOUT_LOGGER_INFO("script %s deleted successfully", name);
 
+#if !defined(__GNUC__) || !defined(__clang__)
     sqlite3_finalize(stmt);
+#endif
     return ret;
 
 err:
+    LOG_DB_ERR(db->db);
+#if !defined(__GNUC__) || !defined(__clang__)
     sqlite3_finalize(stmt);
+#endif
     return ret;
 }
 
-void closeDatabase(sqlite3** db) {
-    if (db != NULL && *db != NULL) {
-        sqlite3_close_v2(*db);
-        *db = NULL;
+DbErr closeDatabase(Database* db) {
+    int rc = sqlite3_close_v2(db->db);
+
+    if (rc != SQLITE_OK) {
+        LOG_DB_ERR(db->db);
+        db->db = NULL;
+        return DB_ERR_CLOSE;
     }
+
+    db->db = NULL;
+    return DB_OK;
 }
