149060c10d58d64815e0e0e875b1bf4ee31f6904
Leven Rochana <levenrok@proton.me>
2026-02-09 14:14:17 +0530

    remove obselete tests

--- test/fs/temp.h
+++
@@ -1,7 +0,0 @@
-#ifndef MIMIR_TEST_TEMP_H
-#define MIMIR_TEST_TEMP_H
-
-void test_temp_file_create(void);
-void test_temp_file_delete(void);
-
-#endif  // MIMIR_TEST_TEMP_H
---
+++ test/fs_test.c
@@ -0,0 +1,11 @@
+#include "unity.h"
+
+void setUp() {}
+
+void tearDown() {}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    return UNITY_END();
+}
